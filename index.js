a6048ada7b8b3096ee78d1c5d3f65950e43ea0f6
maurihm_ <mauriciohernandez942@gmail.com>
2026-04-16 10:04:10 -0600

    Update: Configuración de Sequelize y scripts para producción

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
 const server = require('./server');
+const { sequelize } = require('./models');
 const PORT = process.env.PORT || 3300;
-server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+  } catch (error) {
+    console.error('Error starting server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
